67c9e2a328d547ce5c5036b9f95fb458cd4546d0
huy75 <huy.nguyen66@gmail.com>
2021-02-11 16:41:09 +0000

    feat console 0.1

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ Entry point of the command interpreter.
 """
 
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class"""
@@ -15,9 +15,20 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_EOF(self, line):
-        """Quit command to exit the program"""
+        """EOF command to exit the program"""
         print()
         return True
+
+    def do_create(self, cls):
+        """ Creates a new instance of BaseModel"""
+        if not cls:
+            return (print("** class name missing **"))
+        if cls != 'BaseModel':
+            print("** class doesn't exist **")
+
+        new = BaseModel()
+        new.save()
+        print(new.id)
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
