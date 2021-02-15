5a93890e75e3dc01eba3058ea57de77744c064cb
huy75 <huy.nguyen66@gmail.com>
2021-02-15 15:09:28 +0000

    feat setup test file folders

--- test_base_model_dict.py
+++ test_base_model_dict.py
@@ -4,9 +4,10 @@ from models.base_model import BaseModel
 my_model = BaseModel()
 my_model.name = "Holberton"
 my_model.my_number = 89
+print(my_model.id)
 print(my_model)
-my_model.save()
-print(my_model)
+print(type(my_model.created_at))
+print("--")
 my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
