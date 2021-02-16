4fb77c540aaca34d50e5fe5c051afe693493ce01
huy75 <huy.nguyen66@gmail.com>
2021-02-16 16:29:51 +0000

    add attr test

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -22,12 +22,15 @@ class TestReview(unittest.TestCase):
         self.rev_2 = Place()
 
     def test_Review_attributes(self):
-        """Test that Review class has place_id,
-        user_id and text attributes."""
+        """Test that Review class has place_id, user_id and text
+            attributes."""
         new_review = Review()
-        self.assertTrue("place_id" in new_review.__dir__())
-        self.assertTrue("user_id" in new_review.__dir__())
-        self.assertTrue("text" in new_review.__dir__())
+        place_id = getattr(new_review, "place_id")
+        user_id = getattr(new_review, "user_id")
+        text = getattr(new_review, "text")
+        self.assertIsInstance(place_id, str)
+        self.assertIsInstance(user_id, str)
+        self.assertIsInstance(text, str)
 
     def test_attr_base(self):
         """ Test attribut BaseModel """
