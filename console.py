5d9f2c894fa51d651a1f1441ed46142e8a8fb72e
huy75 <huy.nguyen66@gmail.com>
2021-02-16 11:48:42 +0000

    fix typo

--- console.py
+++ console.py
@@ -153,6 +153,7 @@ class HBNBCommand(cmd.Cmd):
     def default(self, line):
         """Method to take care of following commands:
         <class name>.all()
+        <class name>.count()
         <class name>.show(<id>)
         <class name>.destroy(<id>)
         <class name>.update(<id>, <attribute name>, <attribute value>)
@@ -191,16 +192,16 @@ class HBNBCommand(cmd.Cmd):
                 param = cmd[1].split(',')
                 attr_name = param[1].replace('"', "")
                 attr_val = param[2].replace('"', "").replace(' ', "")
-                args = class_name + " " + id_name + " "
-                + attr_name + " " + attr_val
+                args = class_name + " " + id_name + " " +\
+                    attr_name + " " + attr_val
                 return(fct(args))
             else:
                 my_attr = json.loads(param[1])
                 for k, v in my_attr.items():
                     attr_name = k
                     attr_val = str(v)
-                    args = class_name + " " + id_name + " "
-                    + attr_name + " " + attr_val
+                    args = class_name + " " + id_name + " " +\
+                        attr_name + " " + attr_val
                     fct(args)
                 return
 
